85ad32221829c3440a6107e884bd3841cba3f1a5
Yakub Ali <mamuzaan@yahoo.com>
2023-06-26 00:28:16 +0000

    Added post model

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,29 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, default='1')
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blog_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
